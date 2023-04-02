36ce2a6fe30ba5f2e7ecfc6c2f39d8c185254102
BlisS <bliss@BlisSs-MacBook-Pro.local>
2023-04-02 12:17:08 -0600

    initial commit 🍿

---
+++ main.js
@@ -0,0 +1,125 @@
+const canvas = document.createElement("canvas");
+canvas.id="main-canvas"
+const next = document.querySelector('#next');
+const ctxNext = next.getContext('2d')
+const container = document.querySelector(".canvas-container");
+container.appendChild(canvas);
+const ctx = canvas.getContext("2d");
+console.log("width: ", container.getBoundingClientRect().width)
+canvas.width = container.getBoundingClientRect().width;
+canvas.height = container.getBoundingClientRect().height
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+// instances
+let frames=0
+let requestId;
+let time = null;
+let board = new Board(ctx, ctxNext);
+const moves = {
+    [KEY.LEFT]: (p) => ({ ...p, x: p.x - 1 }),
+    [KEY.RIGHT]: (p) => ({ ...p, x: p.x + 1 }),
+    [KEY.DOWN]: (p) => ({ ...p, y: p.y + 1 }),
+    [KEY.SPACE]: (p) => ({ ...p, y: p.y + 1 }),
+    [KEY.UP]: (p) => board.rotate(p, ROTATION.RIGHT),
+    [KEY.Q]: (p) => board.rotate(p, ROTATION.LEFT)
+  };
+  let accountValues = {
+    score: 0,
+    level: 0,
+    lines: 0
+  };
+  let account = new Proxy(accountValues, {
+    set: (target, key, value) => {
+      target[key] = value;
+      updateAccount(key, value);
+      return true;
+    }
+  });
+  
+  function updateAccount(key, value) {
+    let element = document.getElementById(key);
+    if (element) {
+      element.textContent = value;
+    }
+  }
+  
+
+
+function initNext() {
+    // Calculate size of canvas from constants.
+    ctxNext.canvas.width = 4 * BLOCK_SIZE;
+    ctxNext.canvas.height = 4 * BLOCK_SIZE;
+    ctxNext.scale(BLOCK_SIZE, BLOCK_SIZE);
+  }
+
+  function resetGame() {
+    account.score = 0;
+    account.lines = 0;
+    account.level = 0;
+    board.reset();
+    time = { 
+        start: performance.now(), 
+        elapsed: 0,
+         level: LEVEL[account.level]
+         };
+}
+
+const animate = (now=0) => {
+    frames++;
+    console.log("running");
+    time.elapsed = now-time.start;
+    if(time.elapsed > time.level){
+        time.start = now;
+        if(!board.drop()){     // moving the game
+            //gameover
+            return;
+        }
+    }
+
+    //clear board first
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    board.draw();
+    requestId = requestAnimationFrame(animate)
+}
+
+const pause = () => {
+    if(!requestId) {
+        document.querySelector('#play-btn').textContent='Pausar'
+        animate();
+        //sound
+        return;
+    }
+    cancelAnimationFrame(requestId);
+    requestId=null;
+    ctx.fillStyle = 'black';
+    ctx.fillRect(1, 3, 8, 1.2);
+    ctx.font = '1px Arial';
+    ctx.fillStyle = 'yellow';
+    ctx.fillText('PAUSED', 3, 4);
+
+}
+
+
+
+const play = () => {
+    if(requestId){
+        cancelAnimationFrame(requestId);
+    }
+    animate();
+    document.querySelector('#play-btn').textContent='Pausar'
+}
+
+// event listeners
+document.querySelector('#play-btn').onclick=()=>{
+    if(
+        document.querySelector('#play-btn').textContent === 'Pausar' 
+    ){
+       pause() 
+    }else{
+
+        play();
+    }
+}
+
+initNext();
+resetGame();
